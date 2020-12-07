4eb97fe4372e4634593678b78b2db991b1b6e616
Sara Nolen <nolensara1@gmail.com>
2020-12-07 22:15:37 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,12 +1,18 @@
 require 'pry'
 
-def my_find(collection)
+def my_all?(collection)
   i = 0
+  block_return_values = []
   while i < collection.length
-  return collection[i] if yield(collection[i])
-  i += 0
-end
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+ 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
 
-collection = (1..100).to_a
-my_find(collection){|i| i % 3 == 0 and i % 5 == 0}
+my_all?([1,2,3]) {|i| i < 2}
