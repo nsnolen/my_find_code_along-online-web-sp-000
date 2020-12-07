643ad980dfc86b0339e47c6f344f23556bdb8d99
Sara Nolen <nolensara1@gmail.com>
2020-12-07 21:00:25 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,6 +4,10 @@ def my_find(collection)
   i = 0
   while i < collection.length
     i += 0
-    yield(collection[i])
+  if yield(collection[i])
+    return collection[i]
   end
 end
+
+collection = (1..100).to_a
+my_find(collection){|i| i % 3 == 0 and i % 5 == 0}
